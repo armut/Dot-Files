69aee991eb6d3c0f38b8267212d9b1aef60b2e5c
armut <abdullahakalin@gmail.com>
2020-01-08 20:11:53 +0300

    GPD Pocket 2 related configuration files appended.

---
+++ gpd-pocket2/i3/scripts/brightness.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Sreen brightness controls
+bindsym XF86MonBrightnessUp exec xbacklight -inc 3 # increase screen brightness
+bindsym XF86MonBrightnessDown exec xbacklight -dec 3 # decrease screen brightness
