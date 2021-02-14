4ea75656b8147df14c8fd70bc5598b8a5f4eb7e0
abdullah <abdullah.akalin@gazi.edu.tr>
2021-02-14 21:02:29 +0300

    i3 and polybar config updates.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -10,3 +10,13 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 polybar -c ~/.config/polybar/config.ini main &
+
+# Setup for multiple screens
+#if type "xrandr"; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#       MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload main &
+#   done
+#else
+    #polybar -c ~/.config/polybar/config.ini --reload main &
+#fi
+
